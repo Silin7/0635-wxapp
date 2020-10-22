bf66bdc54b46745026c8f85ccb631fd15c66a579
silin.wang <wsl2535620700@qq.com>
2020-10-22 11:20:30 +0800

    openid sk

--- app.js
+++ app.js
@@ -1,9 +1,32 @@
+//app.js
+import esRequest from "./utils/esRequest"
+
 App({
   onLaunch: function () {
     // 登录
     wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
+      success: (res) => {
+        if (res.code) {
+          let data = {
+            appid: 'wx6fef8f853abfa20e',
+            secret: '637d5ad0a04af65fe13856021aa6c468',
+            code: res.code,
+            grant_type: 'authorization_code'
+          }
+          let URL = `https://api.weixin.qq.com/sns/jscode2session?appid=${data.appid}&secret=${data.secret}&js_code=${data.code}&grant_type=${data.grant_type}`
+          wx.request({
+            method: 'GET',
+            url: URL,
+            data: '',
+            success(res) {
+              wx.setStorageSync('openid', res.data.openid)
+              wx.setStorageSync('session_key', res.data.session_key)
+            },
+            fail(err) {
+              console.warn('fail' + JSON.stringify(err))
+            }
+          })
+        }
       }
     })
     // 获取用户信息
@@ -15,7 +38,7 @@ App({
             success: res => {
               // 可以将 res 发送给后台解码出 unionId
               this.globalData.userInfo = res.userInfo
-
+              
               // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
               // 所以此处加入 callback 以防止这种情况
               if (this.userInfoReadyCallback) {
