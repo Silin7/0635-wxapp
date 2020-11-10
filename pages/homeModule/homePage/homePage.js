454079cbec861c001bfd38dbf85642fdae5aa22f
silin.wang <wsl2535620700@qq.com>
2020-11-10 11:46:26 +0800

    homePage

--- pages/homeModule/homePage/homePage.js
+++ pages/homeModule/homePage/homePage.js
@@ -3,9 +3,16 @@ Page({
   data: {
     active: 0,
     icon: {
-      // normal: 'https://img.yzcdn.cn/vant/user-inactive.png',
-      normal: '/images/homeMoudle/home.png',
-      active: '/images/homeMoudle/home(5).png',
+      home_normal: '/images/homeMoudle/home_normal.png',
+      home_active: '/images/homeMoudle/home_active.png',
+      compass_normal: '/images/homeMoudle/compass_normal.png',
+      compass_active: '/images/homeMoudle/compass_active.png',
+      release_normal: '/images/homeMoudle/release_normal.png',
+      release_active: '/images/homeMoudle/release_active.png',
+      news_normal: '/images/homeMoudle/news_normal.png',
+      news_active: '/images/homeMoudle/news_active.png',
+      mine_normal: '/images/homeMoudle/mine_normal.png',
+      mine_active: '/images/homeMoudle/mine_active.png',
     },
   },
 
