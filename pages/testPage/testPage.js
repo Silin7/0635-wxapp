e5810170519408173983802a6104f99a179930c5
silin.wang <wsl2535620700@qq.com>
2020-12-01 16:05:24 +0800

    视频播放测试

---
+++ pages/testPage/testPage.js
@@ -0,0 +1,13 @@
+Page({
+  data: {
+  },
+  onLoad: function (options) {
+  },
+  onReady: function () {
+  },
+  onShow: function () {
+  },
+  videoErrorCallback: function (e) {
+    console.log('视频错误信息:'+e.detail.errMsg)
+  }
+})
