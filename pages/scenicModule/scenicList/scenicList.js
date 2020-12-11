3f442f575a4a639279f9814656cdac7e431264cd
silin.wang <wsl2535620700@qq.com>
2020-12-11 10:52:18 +0800

    景点列表

--- pages/scenicModule/scenicList/scenicList.js
+++ pages/scenicModule/scenicList/scenicList.js
@@ -6,6 +6,9 @@ Page({
     windowWidth: 0,
     windowHeight: 0,
     scenicActive: 0,
+    totalCount: 0,
+    scenicspotPage: 1,
+    scenicspotLimit: 10,
     scenicspotPosition: '',
     scenicSpotList: []
   },
@@ -21,8 +24,13 @@ Page({
   },
   // 切换Tabs
   scenicChange: function (e) {
+    this.data.scenicspotPage = 1
+    this.data.scenicSpotList = []
     this.data.scenicspotPosition = e.detail.title
     this.getScenicSpot()
+    // wx.pageScrollTo({
+    //   scrollTop: 0
+    // })
   },
   // 景点列表
   getScenicSpot: function () {
@@ -30,12 +38,15 @@ Page({
       this.data.scenicspotPosition = ''
     }
     let data = {
+      page: this.data.scenicspotPage,
+      limit: this.data.scenicspotLimit,
       scenicspot_position: this.data.scenicspotPosition
     }
     esRequest('scenicspot_list', data).then(res => {
       if (res && res.data.code == 0) {
         this.setData({
-          scenicSpotList: res.data.data
+          totalCount: res.data.totalCount,
+          scenicSpotList: this.data.scenicSpotList.concat(res.data.data)
         })
       } else {
         Toast.fail('系统错误')
@@ -48,4 +59,11 @@ Page({
       url: '/pages/scenicModule/scenicDetails/scenicDetails?id=' + e.currentTarget.dataset.item.id
     })
   },
+  // 触底函数
+  onScrollBottom: function () {
+    if (this.data.totalCount > this.data.scenicSpotList.length) {
+      this.data.scenicspotPage += 1
+      this.getScenicSpot()
+    }
+  }
 })
