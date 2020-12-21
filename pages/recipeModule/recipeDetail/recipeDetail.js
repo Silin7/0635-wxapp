060aea54fabb35a34aeb7614d6f7e47a3246b828
silin7 <wsl2535620700@qq.com>
2020-12-21 22:56:24 +0800

    菜谱详情

--- pages/recipeModule/recipeDetail/recipeDetail.js
+++ pages/recipeModule/recipeDetail/recipeDetail.js
@@ -6,12 +6,7 @@ Page({
     windowWidth: 0,
     windowHeight: 0,
     menuId: '',
-    title: '',
-    photo: '',
-    // 食材
-    major: [],
-    // 步骤
-    cookstep: [],
+    menu_details: {}
   },
   onLoad: function (options) {
     this.data.menuId = options.id ? options.id : '1406803'
@@ -33,14 +28,8 @@ Page({
     esRequest('recipe_detail', data).then(res => {
       if (res && res.data.state === 'success') {
         this.setData({
-          title: res.data.result.recipe.title,
-          photo: res.data.result.recipe.original_photo_path,
-          major: res.data.result.recipe.major,
-          cookstep: res.data.result.recipe.cookstep
+          menu_details: res.data.result.recipe
         })
-        console.log(this.data.title)
-        console.log(this.data.major)
-        console.log(this.data.cookstep)
       } else {
         Toast.fail('系统错误')
       }
