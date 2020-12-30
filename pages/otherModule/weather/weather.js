5e5879badac3c11b2f5c67b9ccdd3b7a21d33c56
silin.wang <wsl2535620700@qq.com>
2020-12-30 17:11:28 +0800

    天气

--- pages/otherModule/weather/weather.js
+++ pages/otherModule/weather/weather.js
@@ -12,13 +12,17 @@ Page({
     position: '聊城市',
     cWidth: '',
     cHeight: '',
+    maxData: '',
+    minData: '',
+    weatherImg: '',
+    current: {},
+    forecasts: {},
   },
 
   onLoad: function (options) {
     _self=this;
     this.cWidth = wx.getSystemInfoSync().windowWidth;
     this.cHeight = 500 / 750 * wx.getSystemInfoSync().windowWidth;
-    this.getServerData()
   },
 
   onReady: function () {
@@ -30,92 +34,112 @@ Page({
 
   onShow: function () {
     this.weatherCurrent()
-    this.weatherForecast()
+    this.waitForecast()
   },
 
-  // 配置图标基本信息
-  showColumn(canvasId, chartData) {
-    canvaColumn = new uCharts({
-      $this: _self,
-      canvasId: canvasId,
-      // padding: [15,15,4,15],
-      type: 'area',
-      legend: true,
-      fontSize: 11,
-      background: '#FFFFFF',
-      pixelRatio: 1,
-      animation: true,
-      categories: chartData.categories,
-      series: chartData.series,
-      xAxis: {
-        disableGrid: true,
-        // gridColor: '#4545FF',
-        data: {
-          axisLineColor: '#4545FF'
-        }
-      },
-      yAxis: {
-        gridType: 'dash',
-        // disableGrid: true,
-        // showTitle: true,
-        // data: {
-        //   axisLineColor: '#4545FF',
-        //   title: '℃'
-        // }
-      },
-      dataLabel: true,
-      width: _self.cWidth ,
-      height: _self.cHeight ,
-      extra: {
-        column: {
-          type: 'group',
-          width: _self.cWidth * 0.45 / chartData.categories.length
-        }
-      }
-    });
-  },
-  showColumn2(canvasId, chartData) {
-    canvaColumn = new uCharts({
-      $this: _self,
-      canvasId: canvasId,
-      // padding: [15,15,4,15],
-      type: 'area',
-      legend: true,
-      fontSize: 11,
-      background: '#FFFFFF',
-      pixelRatio: 1,
-      animation: true,
-      categories: chartData.categories,
-      series: chartData.series,
-      xAxis: {
-        disableGrid: true,
-        // gridColor: '#4545FF',
-        data: {
-          axisLineColor: '#4545FF'
-        }
-      },
-      yAxis: {
-        gridType: 'dash',
-        // disableGrid: true,
-        // showTitle: true,
-        // data: {
-        //   axisLineColor: '#4545FF',
-        //   title: '℃'
-        // }
-      },
-      dataLabel: true,
-      width: _self.cWidth ,
-      height: _self.cHeight ,
-      extra: {
-        column: {
-          type: 'group',
-          width: _self.cWidth * 0.45 / chartData.categories.length
-        }
-      }
-    });
+  async waitForecast () {
+    await this.weatherForecast()
+    this.getServerData()
   },
 
-  // 填充图标数据
+  // 获取特定城市今日天气信息
+  weatherCurrent: function () {
+    let data = {
+      position: this.data.position
+    }
+    esRequest('weather_current', data).then(res => {
+      if (res && res.data.code == 1) {
+        this.setData({
+          current: res.data.data
+        })
+        let array = res.data.data.weather.split('')
+        for (let i = 0; i < array.length; i++) {
+          if (array[i] == '转') {
+            this.setData({
+              weatherImg: '/images/otherMoudle/weather_zhuan.png'
+            })
+            continue;
+          }
+          if (array[i] == '晴') {
+            this.setData({
+              weatherImg: '/images/otherMoudle/weather_qing.png'
+            })
+            continue;
+          }
+          if (array[i] == '阴') {
+            this.setData({
+              weatherImg: '/images/otherMoudle/weather_yin.png'
+            })
+            continue;
+          }
+          if (array[i] == '雾') {
+            this.setData({
+              weatherImg: '/images/otherMoudle/weather_wu.png'
+            })
+            continue;
+          }
+          if (array[i] == '雨') {
+            this.setData({
+              weatherImg: '/images/otherMoudle/weather_yu.png'
+            })
+            continue;
+          }
+          if (array[i] == '雪') {
+            this.setData({
+              weatherImg: '/images/otherMoudle/weather_xue.png'
+            })
+            continue;
+          }
+          if (array[i] == '云') {
+            this.setData({
+              weatherImg: '/images/otherMoudle/weather_yun.png'
+            })
+            continue;
+          }
+        }
+      } else {
+        Toast.fail('系统错误')
+      }
+    })
+  },
+
+  // 获取特定城市今天及未来天气信息
+  weatherForecast: function () {
+    let _this = this
+    let data = {
+      position: this.data.position
+    }
+    return new Promise (async (resolve, reject) => {
+      esRequest('weather_forecast', data).then(res => {
+        if (res && res.data.code == 1) {
+          this.setData({
+            forecasts: res.data.data
+          })
+          let dayTemp = []
+          let nightTemp = []
+          res.data.data.forecasts.forEach(item => {
+            let dayTemp2 = item.dayTemp.split('')
+            dayTemp2.pop()
+            dayTemp.push(Number(dayTemp2.join('')))
+            let nightTemp2 = item.nightTemp.split('')
+            nightTemp2.pop()
+            nightTemp.push(Number(nightTemp2.join('')))
+          })
+          _this.data.maxData = Math.max(...dayTemp)
+          _this.data.minData = Math.min(...nightTemp)
+          this.setData({
+            maxData: Math.max(...dayTemp) + 1,
+            minData: Math.min(...nightTemp) - 1
+          })
+        } else {
+          Toast.fail('系统错误')
+        }
+        resolve()
+      })
+    })
+  },
+
+  // 图标信息
   getServerData: function () {
     let data = {
       position: this.data.position
@@ -160,59 +184,51 @@ Page({
         Toast.fail('系统错误')
       }
     })
-    wx.request({
-      url: 'https://www.ucharts.cn/data.json',
-      data: {
-      },
-      success: function (res) {
-        let Column = { categories: [], series: [] };
-        Column.categories = res.data.data.ColumnB.categories;
-        Column.series = res.data.data.ColumnB.series;
-        //自定义标签颜色和字体大小
-        Column.series[1].textColor = 'red';
-        Column.series[1].textSize = 11;
-        _self.showColumn2("canvasColumn2", Column);
-      },
-      fail: () => {
-        console.log("请点击右上角【详情】，启用不校验合法域名");
-      },
-    })
-  },
-  
-  // 获取特定城市今日天气信息
-  weatherCurrent: function () {
-    let data = {
-      position: this.data.position
-    }
-    esRequest('weather_current', data).then(res => {
-      if (res && res.data.code == 1) {
-        console.log(res)
-      } else {
-        Toast.fail('系统错误')
-      }
-    })
   },
 
-  // 获取特定城市今天及未来天气信息
-  weatherForecast: function () {
-    let data = {
-      position: this.data.position
-    }
-    esRequest('weather_forecast', data).then(res => {
-      if (res && res.data.code == 1) {
-        console.log(res)
-      } else {
-        Toast.fail('系统错误')
+  // 绘图
+  showColumn(canvasId, chartData) {
+    canvaColumn = new uCharts({
+      $this: _self,
+      canvasId: canvasId,
+      padding: [20, 10, 5, 10],
+      type: 'area',
+      legend: true,
+      fontSize: 11,
+      background: '#FFFFFF',
+      pixelRatio: 1,
+      animation: true,
+      categories: chartData.categories,
+      series: chartData.series,
+      xAxis: {
+        disableGrid: true,
+        // gridColor: '#4545FF',
+        data: {
+          axisLineColor: '#4545FF'
+        }
+      },
+      yAxis: {
+        gridType: 'dash',
+        format: (val) => { return val.toFixed(1) + '℃' },
+        max: this.data.maxData,
+        min: this.data.minData,
+      },
+      dataLabel: true,
+      width: _self.cWidth ,
+      height: _self.cHeight ,
+      extra: {
+        column: {
+          type: 'group',
+          width: _self.cWidth * 0.45 / chartData.categories.length
+        }
       }
-    })
+    });
   }
 })
 
-// address 	字符串 	城市具体信息，比如 “广东省 深圳市”
-// cityCode 	字符串 	城市code
-// temp 	字符串 	温度值
-// weather 	字符串 	天气描述，具体描述请查看附件，天气描述清单
-// windDirection 	字符串 	风向描述，具体描述请查看附件，风向表清单
-// windPower 	字符串 	风力描述，具体描述请查看附件，风力表清单
-// humidity 	字符串 	湿度值
-// reportTime 	字符串 	此次天气发布时间
+// temp  	温度值
+// weather  	天气描述，具体描述请查看附件，天气描述清单
+// windDirection  	风向描述，具体描述请查看附件，风向表清单
+// windPower  	风力描述，具体描述请查看附件，风力表清单
+// humidity  	湿度值
+// reportTime  	此次天气发布时间
