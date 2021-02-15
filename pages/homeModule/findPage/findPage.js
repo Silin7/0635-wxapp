ce21de62e465fed158c9e1abb0726bd5697c1605
silin7 <wsl2535620700@qq.com>
2021-02-15 12:30:39 +0800

    发现

--- pages/homeModule/findPage/findPage.js
+++ pages/homeModule/findPage/findPage.js
@@ -6,17 +6,10 @@ Page({
     id_key: '',
     windowWidth: 0,
     windowHeight: 0,
-    tabActive: 0,
     dynamicPage: 1,
     dynamicLimit: 10,
     totalCount: 0,
-    dynamicList: [],
-    sidebarKey: 0,
-    newsTypeList: [],
-    newsNext: true,
-    typeId: '',
-    newsPage: 1,
-    newsList: [],
+    dynamicList: []
   },
 
   onLoad: function (options) {
@@ -27,47 +20,10 @@ Page({
       windowWidth: wx.getSystemInfoSync().windowWidth,
       windowHeight: wx.getSystemInfoSync().windowHeight
     })
+    this.getDynamicList()
   },
 
   onShow: function () {
-    // 切换底部tabs清空
-    this.setData({
-      dynamicList: [],
-      newsList: [],
-    })
-    this.getDynamicList()
-    this.awaitNews()
-  },
-
-  async awaitNews () {
-    await this.getNewsType()
-    this.data.typeId = this.data.newsTypeList[0].type_id
-    this.getNewsList()
-  },
-
-  // tabs切换
-  tabChange: function (event) {
-    this.setData({
-      tabActive: event.detail.name
-    })
-    if (event.detail.name === 0) {
-      this.setData({
-        dynamicPage: 1,
-        totalCount: 0,
-        dynamicList: [],
-      })
-      this.getDynamicList()
-    }
-  },
-
-  // sidebar切换
-  sidebarChange: function (event) {
-    this.setData({
-      typeId: this.data.newsTypeList[event.detail].type_id,
-      newsPage: 1,
-      newsList: [],
-    })
-    this.getNewsList()
   },
   
   // 同城动态列表
@@ -100,8 +56,8 @@ Page({
     })
   },
 
-  // 同城动态函数
-  onScrollBottom1: function () {
+  // 同城动态触底函数
+  onScrollBottom: function () {
     if (this.data.totalCount > this.data.dynamicList.length) {
       this.data.dynamicPage += 1
       this.getDynamicList()
@@ -123,53 +79,6 @@ Page({
       })
     }
     // 03：普通
-  },
-
-  // 新闻类型列表
-  getNewsType: function () {
-    return new Promise (async (resolve, reject) => {
-      esRequest('classification_news').then(res => {
-        if (res && res.data.code === 0) {
-          this.setData({
-            newsTypeList: res.data.data
-          })
-        } else {
-          Toast.fail('系统错误')
-        }
-        resolve()
-      })
-    })
-  },
-
-  // 新闻列表
-  getNewsList: function () {
-    let data = {
-      typeId: this.data.typeId,
-      page: this.data.newsPage
-    }
-    esRequest('news_list', data).then(res => {
-      if (res && res.data.code == 1) {
-        this.setData({
-          newsList: this.data.newsList.concat(res.data.data)
-        })
-      } else {
-        this.data.newsNext = false
-      }
-    })
-  },
-
-  // 新闻触底函数
-  onScrollBottom2: function () {
-    if (this.data.newsNext) {
-      this.data.newsPage += 1
-      this.getNewsList()
-    }
-  },
-
-  // 新闻详情
-  getNewsDetalis: function (e) {
-    wx.navigateTo({
-      url: '/pages/dynamicModule/newsDetails/newsDetails?newsId=' + e.currentTarget.dataset.item.newsId
-    })
   }
+
 })
