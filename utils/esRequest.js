623dcbabdd4a2cf28f5fcc08fc9954424994a66b
silin.wang <wsl2535620700@qq.com>
2021-02-19 10:11:12 +0800

    拯救不开心接口

--- utils/esRequest.js
+++ utils/esRequest.js
@@ -1,6 +1,7 @@
 import baseURL from "./baseURL"
 import classificationRequest from "./sub-unitRequest/classificationRequest"
 import dynamicRequest from "./sub-unitRequest/dynamicRequest"
+import happyRequest from "./sub-unitRequest/happyRequest"
 import historyRequest from "./sub-unitRequest/historyRequest"
 import loginRequest from "./sub-unitRequest/loginRequest"
 import marryRequest from "./sub-unitRequest/marryRequest"
@@ -12,10 +13,9 @@ import recipeRequest from "./sub-unitRequest/recipeRequest"
 import recordRequest from "./sub-unitRequest/recordRequest"
 import scenicspotRequest from "./sub-unitRequest/scenicspotRequest"
 import specialtyRequest from "./sub-unitRequest/specialtyRequest"
-import treasureChest from "./sub-unitRequest/treasureChest"
 
 export default (localObjects, parametersData, parameters) => {
-  var globalObjects = Object.assign({}, classificationRequest, dynamicRequest, historyRequest, loginRequest, marryRequest, messageRequest, mineRequest, otherRequest, pictureRequest, recipeRequest, recordRequest, scenicspotRequest, specialtyRequest, treasureChest)
+  var globalObjects = Object.assign({}, classificationRequest, dynamicRequest, historyRequest, loginRequest, marryRequest, messageRequest, mineRequest, otherRequest, pictureRequest, recipeRequest, recordRequest, scenicspotRequest, specialtyRequest, happyRequest)
   var apiMethod = globalObjects[localObjects].apiMethod
   if (parameters) {
     var requestURL = baseURL.baseURL + globalObjects[localObjects].apiName + '/' + parameters
---
+++ utils/sub-unitRequest/happyRequest.js
@@ -0,0 +1,8 @@
+let happyRequest = {
+  story_list: {
+    apiMethod: 'GET',
+    apiName: '/happy/story_list',
+    name: '段子列表（参数：type_id）'
+  },
+}
+module.exports = happyRequest
