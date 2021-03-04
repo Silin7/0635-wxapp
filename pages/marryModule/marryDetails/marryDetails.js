0dfd34d89633224d64f95e94bc291a13e72da819
silin.wang <wsl2535620700@qq.com>
2021-03-04 17:23:11 +0800

    相亲模块

--- pages/marryModule/marryDetails/marryDetails.js
+++ pages/marryModule/marryDetails/marryDetails.js
@@ -10,16 +10,13 @@ Page({
     swiperList: [],
     personDetails: {},
     userInfo: {},
-    dialogType: '',
     dialogShow: false,
-    dialogTitle: '',
-    dialogText: '',
     dialogButtons: [{text: '取消'}, {text: '确定'}],
   },
 
   onLoad: function (options) {
     this.data.id_key = wx.getStorageSync('id_key').toString()
-    this.data.register_id = options.register_id ? options.register_id : '2'
+    this.data.register_id = options.register_id ? options.register_id : ''
   },
 
   onReady: function () {
@@ -89,36 +86,31 @@ Page({
   // 关注Ta
   btnGz: function () {
     this.setData({
-      dialogType: '01',
-      dialogShow: true,
-      dialogTitle: '关注TA',
-      dialogText: '您确定要关注Ta吗？',
+      dialogShow: true
     })
   },
 
   // 要微信 / 求约会
   btnWY: function (e) {
-    // 要微信
-    if (e.currentTarget.dataset.type === '01') {
-      this.setData({
-        dialogType: '02',
-        dialogShow: true,
-        dialogTitle: '要微信',
-        dialogText: '您确定要Ta的微信嘛？',
-      })
+    let data = {
+      register_id: this.data.personDetails.register_id,
+      followers_id: this.data.userInfo.id
     }
-    // 求约会
-    if (e.currentTarget.dataset.type === '02') {
-      this.setData({
-        dialogType: '03',
-        dialogShow: true,
-        dialogTitle: '推自己',
-        dialogText: '系统会将您个人中心绑定的手机号作为微信推送给对方，请确认您的手机号是否正确',
-      })
-    }
+    esRequest('marry_issign', data).then(res => {
+      if (res && res.data.code === 0) {
+        if (res.data.type === '0') {
+          wx.navigateTo({
+            url: `/pages/marryModule/marryRegistration/marryRegistration?receiver_id=${this.data.personDetails.user_id}&register_id=${this.data.personDetails.register_id}&gender=${this.data.personDetails.gender}`
+          })
+        }
+        if (res.data.type === '1') {
+          Toast.success('您已经报名')
+        }
+      }
+    })
   },
 
-  // dialog确定按钮
+  // 关注确定按钮
   tapDialogButton: function (e) {
     this.setData({
       dialogShow: false
@@ -128,98 +120,26 @@ Page({
     }
     if (e.detail.index === 1) {
       let _this = this
-      // 01: 关注TA
-      if (this.data.dialogType === '01') {
-        let data = {
-          followers_id: _this.data.id_key,
-          watched_id: _this.data.personDetails.user_id,
-          nick_name: _this.data.personDetails.nick_name,
-          photo: _this.data.personDetails.photo1,
-          introduce: _this.data.personDetails.introduce
-        }
-        esRequest('follow_users', data).then(res => {
-          if (res && res.data.code === 0) {
-            wx.setStorageSync('tp_key', '04')
-            if (res.data.type === '1') {
-              Toast.success('已关注Ta啦')
-            }
-            if (res.data.type === '0') {
-              Toast.success('关注成功')
-            }
-          } else {
-            Toast.fail('系统错误')
-          }
-        })
+      let data = {
+        followers_id: _this.data.id_key,
+        watched_id: _this.data.personDetails.register_id,
+        nick_name: _this.data.personDetails.nick_name,
+        photo: _this.data.personDetails.photo1,
+        introduce: _this.data.personDetails.introduce
       }
-      // 02: 要微信
-      if (this.data.dialogType === '02') {
-        let message_title = ''
-        let message_content = ''
-        if (this.data.personDetails.gender === '男') {
-          message_title = '小哥哥，可以给个微信吗？'
-          message_content = `你好，能给我你的微信吗？我被你的颜值所打动，在我心里你一定是个乐观开朗的男孩子。但我不能自己妄加推断，所以想了解一下你真的性格。`
-        }
-        if (this.data.personDetails.gender === '女') {
-          message_title = '小姐姐，可以给个微信吗？'
-          message_content = `你好，能给我你的微信吗？我被你的颜值所打动，在我心里你一定是个善良温柔的女孩子。但我不能自己妄加推断，所以想了解一下你真的性格。`
-        }
-        let data = {
-          receiver_id: this.data.personDetails.user_id,
-          sender_id: this.data.id_key,
-          sender_name: this.data.userInfo.nickName,
-          sender_img: this.data.userInfo.avatarUrl,
-          message_title: message_title,
-          message_content: message_content,
-          message_type: '01'
-        }
-        esRequest('permessage_send', data).then(res => {
-          if (res && res.data.code === 0) {
-            Toast.success('发送成功')
-            if (res.data.type === '1') {
-              Toast.success('发送过消息啦')
-            }
-            if (res.data.type === '0') {
-              Toast.success('发送成功')
-            }
-          } else {
-            Toast.fail('系统错误')
+      esRequest('follow_users', data).then(res => {
+        if (res && res.data.code === 0) {
+          wx.setStorageSync('tp_key', '04')
+          if (res.data.type === '1') {
+            Toast.success('已关注Ta啦')
           }
-        })
-      }
-      // 03: 求约会
-      if (this.data.dialogType === '03') {
-        let message_title = ''
-        let message_content = ''
-        if (this.data.personDetails.gender === '男') {
-          message_title = '小哥哥，很想和你认识一下呢？'
-          message_content = `你好，我被你的颜值所打动，在我心里你一定是个乐观开朗的男孩子。但我不能自己妄加推断，所以想了解一下你真正的性格。这是我的微信号<span style="color: #4545FF;">${this.data.userInfo.userPhone}</span>，期待我们的缘分鸭。`
-        }
-        if (this.data.personDetails.gender === '女') {
-          message_title = '小姐姐，很想和你认识一下呢？'
-          message_content = `你好，我被你的颜值所打动，在我心里你一定是个善良温柔的女孩子。但我不能自己妄加推断，所以想了解一下你真正的性格。这是我的微信号<span style="color: #4545FF;">${this.data.userInfo.userPhone}</span>，期待我们的缘分鸭。`
-        }
-        let data = {
-          receiver_id: this.data.personDetails.user_id,
-          sender_id: this.data.id_key,
-          sender_name: this.data.userInfo.nickName,
-          sender_img: this.data.userInfo.avatarUrl,
-          message_title: message_title,
-          message_content: message_content,
-          message_type: '02'
-        }
-        esRequest('permessage_send', data).then(res => {
-          if (res && res.data.code === 0) {
-            if (res.data.type === '1') {
-              Toast.success('发送过消息啦')
-            }
-            if (res.data.type === '0') {
-              Toast.success('发送成功')
-            }
-          } else {
-            Toast.fail('系统错误')
+          if (res.data.type === '0') {
+            Toast.success('关注成功')
           }
-        })
-      }
+        } else {
+          Toast.fail('系统错误')
+        }
+      })
     }
   }
 })
---
+++ pages/marryModule/marryRegistration/marryRegistration.js
@@ -0,0 +1,199 @@
+import regular from '../../../utils/regular'
+import esRequest from '../../../utils/esRequest';
+import Toast from '../../../miniprogram_npm/vant-weapp/toast/toast';
+
+Page({
+  data: {
+    windowWidth: 0,
+    windowHeight: 0,
+    register_id: '',
+    gender: '',
+    nickName: '',
+    userInfo: {},
+    dataType: '01',
+    dataForm: {
+      receiver_id: '',
+      sender_id: '',
+      sender_name: '',
+      sender_img	: '',
+      message_title: '',
+      message_content: '',
+      message_type: '02',
+    },
+    dialogTitle: '期待缘分发生',
+    dialogText: '您确定要对方的微信吗？',
+    dialogShow: false,
+    dialogButtons: [{text: '取消'}, {text: '确定'}],
+  },
+
+  onLoad: function (option) {
+    console.log(option)
+    if (option.receiver_id) {
+      this.data.dataForm.receiver_id = option.receiver_id
+    }
+    if (option.register_id) {
+      this.data.register_id = option.register_id
+    }
+    if (option.gender) {
+      this.data.gender = option.gender
+    }
+    if (wx.getStorageSync('userIfo')) {
+      this.setData({
+        nickName: wx.getStorageSync('userIfo').nickName,
+        userInfo: wx.getStorageSync('userIfo')
+      })
+    } else {
+      this.getMineInfo()
+    }
+  },
+
+  onReady: function () {
+    this.setData({
+      windowWidth: wx.getSystemInfoSync().windowWidth,
+      windowHeight: wx.getSystemInfoSync().windowHeight
+    })
+  },
+
+  // 填写姓名
+  nickNameTap: function (event) {
+    this.data.userInfo.nickName = event.detail.value
+    this.setData({
+      userInfo: this.data.userInfo
+    });
+  },
+
+  // 类型切换
+  typeChange(event) {
+    if (event.detail === '01') {
+      this.setData({
+        dialogTitle: '期待缘分发生',
+        dialogText: '您确定要对方的微信吗？',
+        dataType: event.detail
+      });
+    }
+    if (event.detail === '02') {
+      this.setData({
+        dialogTitle: '期待缘分发生',
+        dialogText: '您确定将您的微信推送给对方吗？',
+        dataType: event.detail
+      });
+    }
+  },
+
+  // 填写电话
+  userPhoneTap: function (event) {
+    this.data.userInfo.userPhone = event.detail.value
+    this.setData({
+      userInfo: this.data.userInfo
+    });
+  },
+
+  // 获取个人信息
+  getMineInfo: function () {
+    let data = {
+      id: wx.getStorageSync('id_key')
+    }
+    esRequest('mine_info', data).then (res => {
+      if (res && res.data.code === 0) {
+        wx.setStorageSync('userIfo', res.data.data)
+        this.setData({
+          nickName: res.data.data.nickName,
+          userInfo: res.data.data
+        })
+      } else {
+        Toast.fail('系统错误')
+      }
+    })
+  },
+
+  signUp: function () {
+    let data = this.data.userInfo
+    for (let key in data) {
+      if (data[key] == "") {
+        if (key == 'nickName') {
+          Toast.fail('请填写姓名')
+          return
+        }
+        if (key == 'userPhone') {
+          if (this.data.dataType === '02') {
+            Toast.fail('请填写微信')
+            return
+          }
+        }
+        if (key == 'gender') {
+          Toast.fail('请选择性别')
+          return
+        }
+      }
+    }
+    if (this.data.dataType === '01') {
+      this.setData({
+        dialogShow: true
+      })
+    }
+    if (this.data.dataType === '02') {
+      if (regular.phoneNumber(this.data.userInfo.userPhone)) {
+        this.setData({
+          dialogShow: true
+        })
+      } else {
+        Toast.fail('微信格式错误')
+      }
+    }
+  },
+
+  // 确定报名
+  tapDialogButton: function (e) {
+    let _this = this
+    this.setData({
+      dialogShow: false
+    })
+    if (e.detail.index == 1) {
+      let data = {
+        register_id: this.data.register_id,
+        followers_id: this.data.userInfo.id
+      }
+      esRequest('marry_sign', data).then(res => {
+        if (res && res.data.code === 0) {
+          _this.data.dataForm.sender_id = _this.data.userInfo.id
+          _this.data.dataForm.sender_name = _this.data.userInfo.nickName
+          _this.data.dataForm.sender_img = _this.data.userInfo.avatarUrl
+          if (_this.data.gender === '男') {
+            if (_this.data.dataType === '01') {
+              _this.data.dataForm.message_title = '小哥哥，可以留个微信吗？'
+              _this.data.dataForm.message_content = `你好，能给我留一个你的微信吗？我被你的颜值所打动，在我心里你一定是个乐观开朗的男孩子。但我不能自己妄加推断，所以想了解一下你真的性格。`
+            }
+            if (_this.data.dataType === '02') {
+              _this.data.dataForm.message_title = '小哥哥，很想和你认识一下呢'
+              _this.data.dataForm.message_content = `你好，我被你的颜值所打动，在我心里你一定是个乐观开朗的男孩子。但我不能自己妄加推断，所以想了解一下你真正的性格。这是我的微信号<span style="color: #4545FF;">${_this.data.userInfo.userPhone}</span>，期待我们的缘分鸭。`
+            }
+          }
+          if (_this.data.gender === '女') {
+            if (_this.data.dataType === '01') {
+              _this.data.dataForm.message_title = '小姐姐，可以留个微信吗？'
+              _this.data.dataForm.message_content = `你好，能给我留一个你的微信吗？我被你的颜值所打动，在我心里你一定是个善良温柔的女孩子。但我不能自己妄加推断，所以想了解一下你真的性格。`
+            }
+            if (_this.data.dataType === '02') {
+              _this.data.dataForm.message_title = '小姐姐，很想和你认识一下呢'
+              _this.data.dataForm.message_content = `你好，我被你的颜值所打动，在我心里你一定是个善良温柔的女孩子。但我不能自己妄加推断，所以想了解一下你真正的性格。这是我的微信号<span style="color: #4545FF;">${_this.data.userInfo.userPhone}</span>，期待我们的缘分鸭。`
+            }
+          }
+          esRequest('permessage_send', _this.data.dataForm).then(res => {
+            if (res && res.data.code === 0) {
+              Toast.success('发送成功')
+              setTimeout(function () {
+                wx.navigateBack({
+                  delta: 1
+                })
+              }, 1500)
+            } else {
+              Toast.fail('系统错误')
+            }
+          })
+        } else {
+          Toast.fail('系统错误')
+        }
+      })
+    }
+  }
+})
